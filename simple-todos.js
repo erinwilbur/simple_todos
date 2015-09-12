c83050876c89d80f6a568c7acf32c7309e868191
Erin Wilbur <erinmariewilbur@gmail.com>
2015-09-11 22:39:41 -0500

    add events on tasks

--- simple-todos.js
+++ simple-todos.js
@@ -26,4 +26,17 @@ if (Meteor.isClient) {
   		event.target.text.value = "";
   	}
   });
+
+  // Make UI elements do something
+  Template.task.events({
+  	"click .toggle-checked": function() {
+  		// Set the checked property to the opposite of its current val
+  		Tasks.update(this._id, {
+  			$set: {checked: ! this.checked}
+  		});
+  	},
+  	"click .delete": function() {
+  		Tasks.remove(this._id);
+  	}
+  });
 }
